73d7b9c9a706057ce3fe6354811f256083e23d4a
dmKopylets <dm.kopylets@gmail.com>
2024-12-23 17:07:32 +0200

    blank project

---
+++ app/assets/js/app.js
@@ -0,0 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// any CSS you import will output into a single css file (app.css in this case)
+
+import '../css/app.css';
---
+++ app/src/Form/BookType.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Book;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\DataTransformer\TagsToIdsTransformer;
+
+class BookType extends AbstractType
+{
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $tagsArray = [];
+        foreach ($options['tags'] as $tag) {
+            $tagsArray[$tag->getTitle()] = $tag->getId();
+        }
+
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Назва',
+                'attr' => [
+                    'placeholder' => 'Title'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Опис'
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title',
+            ])
+            ->add('tags', ChoiceType::class, [
+                'autocomplete' => true,
+                'choices' => $tagsArray,
+                'label' => 'Select Tags',
+//                'choice_label' => 'title',
+                'multiple' => true,
+                'expanded' => false,
+                'attr' => [
+//                    'class' => 'custom-select-multiple',
+                    'size' => 6,
+                    'style' => 'overflow-y: scroll;',
+                ],
+                'required' => false,
+            ]);
+        $builder->get('tags')->addModelTransformer(new TagsToIdsTransformer($this->entityManager));
+
+    }
+
+//        $builder->addEventListener(
+//            FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+//            $form = $event->getForm();
+//
+//            //$tags = $form->getParent()->getParent()->getData();
+//            $tags = $form->get('tags')->getData();
+//
+//            if ($tags) {
+//                $parameters = json_decode($tags->getParameters(), true);
+
+//                $bankFormData = $parameters['form']['spb-banks'] ?? null;
+
+//                if ($bankFormData) {
+//                    $event->setData([
+//                        'label' => $bankFormData['label'],
+//                        'options' => array_values($bankFormData['options'])
+//                    ]);
+
+//            }
+//        });
+//    }
+
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+            'tags' => [],
+        ]);
+    }
+}
